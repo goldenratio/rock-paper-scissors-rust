17260885f2c5415a623df7aec593e4900dd06e2e
goldenratio <karthikeyanvj@gmail.com>
2023-11-20 23:54:12 +0200

    rename project name

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,10 @@ use crate::handlers::create::create;
 use crate::handlers::game_action::game_action;
 use crate::handlers::game_events::game_events;
 use crate::handlers::join::join;
-use actix_files as fs;
 use actix_files::Files;
-use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 use std::collections::HashMap;
-use std::path::PathBuf;
 use std::sync::Mutex;
-
-#[get("/")]
-async fn index(_req: HttpRequest) -> std::io::Result<fs::NamedFile> {
-    let path: PathBuf = "./client/index.html".parse().unwrap();
-    Ok(fs::NamedFile::open(path)?)
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
